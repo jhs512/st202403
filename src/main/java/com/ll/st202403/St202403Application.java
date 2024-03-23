29493cbfc4e5e637b0506f7e8817c4e957fccfd8
jhs512 <jangka512@gmail.com>
2024-03-23 23:23:14 +0900

    work

---
+++ src/main/java/com/ll/st202403/St202403Application.java
@@ -0,0 +1,13 @@
+package com.ll.st202403;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class St202403Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(St202403Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ll/st202403/St202403ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.st202403;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class St202403ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
